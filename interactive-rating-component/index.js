b12b55536234ebe07905d25eb991c8650e45bad4
Mihir Nayak <99991521+mihir2226@users.noreply.github.com>
2022-08-04 01:40:03 +0530

    project uploaded

---
+++ interactive-rating-component/index.js
@@ -0,0 +1,19 @@
+const ratingContainer = document.querySelector('.rating-container');
+const thanksContainer = document.querySelector('.thank-you');
+const submitButton = document.getElementById('submit');
+const rating = document.getElementById('rating');
+const rates = document.querySelectorAll('.btn');
+
+
+submitButton.addEventListener("click", () => {
+
+    thanksContainer.classList.remove("hidden");
+    ratingContainer.style.display = "none";
+    
+});
+
+rates.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rating.innerHTML = rate.innerHTML;
+    });
+});
